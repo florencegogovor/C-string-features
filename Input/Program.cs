848d2dfda4a2b026a012c61471a09e7ddf279609
Kindeme <fkounoudji@gmail.com>
2020-12-05 21:17:58 -0600

    Add project files.

---
+++ Input/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Input";
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + "!");
+            Console.ReadKey();  
+        }
+    }
+}
